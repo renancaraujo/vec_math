f98c2ef17422894f8ebfb38c2b2d93f07f7e0d79
Renan Araujo <renanmatheuscorreiaaraujo@gmail.com>
2023-06-05 23:21:30 +0100

    remove trailing commas from doc

--- tool/generate_vecs/hooks/post_gen.dart
+++ tool/generate_vecs/hooks/post_gen.dart
@@ -12,4 +12,18 @@ void main() {
     ['format', 'lib/src/vectors/generated'],
     runInShell: true,
   );
+
+  searchAndReplace('lib/src/vectors/generated', ', )', ')');
+}
+
+void searchAndReplace(String path, String searchText, String replaceText) {
+  final dir = Directory(path);
+  dir.listSync(recursive: true).forEach((file) {
+    if (file is File && file.path.endsWith('.dart')) {
+      final contents = file.readAsStringSync();
+      if (contents.contains(searchText)) {
+        file.writeAsStringSync(contents.replaceAll(searchText, replaceText));
+      }
+    }
+  });
 }
